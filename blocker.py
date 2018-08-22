61feefc9e62509d4527ed007f7cfa442684d0782
w3bh4ck <amadiluckysampson@gmail.com>
2018-08-22 05:04:10 +0100

    adding blacklisted sites to the host file

--- blocker.py
+++ blocker.py
@@ -3,13 +3,21 @@ from datetime import datetime as dt
 
 
 host_path = r"C:\Windows\System32\drivers\etc"
-redirect = 127.0.0.1
+host_temp = "hosts"
+redirect = "127.0.0.1"
 blocked_websites = ["www.facebook.com", "facebook.com"]
 
-while true:
-	if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+while True:
+	if dt(dt.now().year, dt.now().month, dt.now().day, 8) > dt.now() > dt(dt.now().year, dt.now().month, dt.now().day, 16):
 		print("This is working hours...")
-		time.sleep(100)
+		with open(host_temp, 'r+') as file:
+			content = file.read()
+			for website in blocked_websites:
+				if website in content:
+					pass
+				else:
+					file.write(redirect+" "+website+"\n")
+
 	else:
 		print("Fun hours!")
-	time.sleep(200)
+	time.sleep(20)
